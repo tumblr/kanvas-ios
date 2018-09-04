7b35318df2f36e6468c785134b9a2bc11ef7aaad
Tony Cheng <tony@getkanvas.com>
2018-09-04 12:26:58 -0400

    updated view controller logic and added permissions

--- KanvasCameraExample/KanvasCameraExample/ViewController.swift
+++ KanvasCameraExample/KanvasCameraExample/ViewController.swift
@@ -5,18 +5,56 @@
 //
 
 import UIKit
+import KanvasCamera
+import Photos
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    let button = UIButton(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .black
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        button.setTitle("Start camera!", for: .normal)
+        button.addTarget(self, action: #selector(cameraSelected), for: .touchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func cameraSelected() {
+        let settings = CameraSettings()
+        settings.enabledModes = [.photo, .gif, .stopMotion]
+        settings.defaultMode = .stopMotion
+        settings.exportStopMotionPhotoAsVideo = true
+        let controller = CameraController(settings: settings, analyticsProvider: KanvasCameraAnalyticsStub())
+        controller.delegate = self
+        self.present(controller, animated: true, completion: .none)
     }
 
+}
 
+extension ViewController: CameraControllerDelegate {
+    func didCreateMedia(media: KanvasCameraMedia?, error: Error?) {
+        if let media = media {
+            switch media {
+            case .image(let url):
+                if let image = UIImage(contentsOfFile: url.path) {
+                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                }
+            case .video(let url):
+                UISaveVideoAtPathToSavedPhotosAlbum(url.path, nil, nil, nil)
+            default: break
+            }
+        }
+        dismiss(animated: true, completion: .none)
+    }
+
+    func dismissButtonPressed() {
+        button.setTitle("It's okay. Try again later!", for: .normal)
+        dismiss(animated: true, completion: .none)
+    }
 }
