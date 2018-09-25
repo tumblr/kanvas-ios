c69b2cd42eb10326ce23f76eb211d13c999f4d35
Tony Cheng <tony@getkanvas.com>
2018-09-25 14:50:39 -0400

    Update pixel buffer

--- Classes/Extensions/AVAssetWriterInputPixelBufferAdaptor+Append.swift
+++ Classes/Extensions/AVAssetWriterInputPixelBufferAdaptor+Append.swift
@@ -7,6 +7,10 @@
 import AVFoundation
 import Foundation
 
+private struct AdaptorConstants {
+    static let MaximumAppendAttempts: Int = 50
+}
+
 extension AVAssetWriterInputPixelBufferAdaptor {
     
     /// method to help append a buffer to an asset writer input
@@ -16,15 +20,22 @@ extension AVAssetWriterInputPixelBufferAdaptor {
     ///   - time: append CMTime
     ///   - completion: Bool for whether the buffer was appended
     func append(buffer: CVPixelBuffer, time: CMTime, completion: @escaping (Bool) -> Void) {
+        append(buffer: buffer, time: time, attempts: 0, completion: completion)
+    }
+    
+    private func append(buffer: CVPixelBuffer, time: CMTime, attempts: Int, completion: @escaping (Bool) -> Void) {
+        if attempts > AdaptorConstants.MaximumAppendAttempts {
+            completion(false)
+            return
+        }
         if assetWriterInput.isReadyForMoreMediaData {
-            completion(append(buffer, withPresentationTime: time))
+            let appended = append(buffer, withPresentationTime: time)
+            completion(appended)
         }
         else {
-            let _ = assetWriterInput.observe(\.isReadyForMoreMediaData) { [unowned self] (writer, change) in
-                if self.assetWriterInput.isReadyForMoreMediaData {
-                    completion(self.append(buffer, withPresentationTime: time))
-                }
-            }
+            performUIUpdateAfter(deadline: .now() + .milliseconds(100), execute: { [unowned self] in
+                self.append(buffer: buffer, time: time, attempts: attempts + 1, completion: completion)
+            })
         }
     }
 }
