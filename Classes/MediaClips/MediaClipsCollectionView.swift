120d4cb11c7fd47cf216aff7781d6e4cc18836cc
Tony Cheng <tony@getkanvas.com>
2018-10-24 15:58:28 -0400

    running ./install.sh and updated constant

--- Classes/MediaClips/MediaClipsCollectionView.swift
+++ Classes/MediaClips/MediaClipsCollectionView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 private struct MediaClipsCollectionViewConstants {
     static var bufferSize: CGFloat = 10
-    static var height: CGFloat = MediaClipsCollectionCell.minimumHeight + 10
+    static var height: CGFloat = MediaClipsCollectionCell.minimumHeight + MediaClipsCollectionViewConstants.bufferSize
 }
 
 /// Collection view for the MediaClipsCollectionController
