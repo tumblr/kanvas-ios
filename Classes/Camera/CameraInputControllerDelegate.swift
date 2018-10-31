56d68aa2bfbbbf1c85519b0a45ab8676af9b7174
Tony Cheng <tony@getkanvas.com>
2018-10-31 18:36:57 -0400

    Revert "Revert "Kanvas Camera - pinch and pan to zoom""

---
+++ Classes/Camera/CameraInputControllerDelegate.swift
@@ -0,0 +1,19 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+
+/// Delegate for handling camera input actions
+protocol CameraInputControllerDelegate {
+    /// Delegate to reset the current device zoom
+    func cameraInputControllerShouldResetZoom()
+    
+    /// Delegate method to set zoom based on pinch
+    ///
+    /// - Parameters:
+    ///   - gesture: the pinch gesture
+    func cameraInputControllerPinched(gesture: UIPinchGestureRecognizer)
+}
