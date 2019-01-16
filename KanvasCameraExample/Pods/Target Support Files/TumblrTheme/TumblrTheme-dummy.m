2cb6effbf6735566f397d41dc79a75f33f0d1430
Benjamin Su <benjamins@tumblr.com>
2019-01-16 11:35:03 -0500

    update kanvas pods

---
+++ KanvasCameraExample/Pods/Target Support Files/TumblrTheme/TumblrTheme-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TumblrTheme : NSObject
+@end
+@implementation PodsDummy_TumblrTheme
+@end
---
+++ KanvasCameraExample/Pods/Target Support Files/TumblrTheme/TumblrTheme-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UIColor+Orangina.h"
+#import "UIFont+ComposeFonts.h"
+#import "UIFont+Orangina.h"
+#import "UIFont+PostFonts.h"
+
+FOUNDATION_EXPORT double TumblrThemeVersionNumber;
+FOUNDATION_EXPORT const unsigned char TumblrThemeVersionString[];
+
