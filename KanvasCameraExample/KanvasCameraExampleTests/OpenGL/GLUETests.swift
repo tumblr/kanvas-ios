b74b24e67bcac51f568a5ed4ef8b02d99275a23a
Jimmy Schementi <jimmys@tumblr.com>
2019-02-20 22:10:59 -0500

    KanvasCamera: fix super violations

--- KanvasCameraExample/KanvasCameraExampleTests/OpenGL/GLUETests.swift
+++ KanvasCameraExample/KanvasCameraExampleTests/OpenGL/GLUETests.swift
@@ -9,23 +9,15 @@ import XCTest
 class GLUETests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
