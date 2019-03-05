7ba907ff90391d764957a057387ba4461821e04c
Tumblr Human <noreply@tumblr.com>
2019-03-05 09:53:24 -0700

    4962 - Add Utils pod to Podfile, run `bundle exec pod install`

--- KanvasCameraExample/Pods/Target Support Files/Pods-KanvasCameraExample/Pods-KanvasCameraExample-frameworks.sh
+++ KanvasCameraExample/Pods/Target Support Files/Pods-KanvasCameraExample/Pods-KanvasCameraExample-frameworks.sh
@@ -155,10 +155,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KanvasCamera/KanvasCamera.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TumblrTheme/TumblrTheme.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Utils/Utils.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KanvasCamera/KanvasCamera.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TumblrTheme/TumblrTheme.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Utils/Utils.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
