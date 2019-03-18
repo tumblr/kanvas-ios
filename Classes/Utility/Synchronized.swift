3ce581b854564e7cf8f913bc50c797bd7c8e18ef
Jimmy Schementi <jimmys@tumblr.com>
2019-03-18 13:30:05 -0400

    SPELLING

--- Classes/Utility/Synchronized.swift
+++ Classes/Utility/Synchronized.swift
@@ -6,12 +6,18 @@
 
 import Foundation
 
+/// Execute `block`, synchronized on `object`.
+/// Multiple blocks synchronized on the same `object`,
+/// but executing on different threads, will not run at the same time.
 func synchronized(_ object: AnyObject, block: () -> Void) {
     objc_sync_enter(object)
     block()
     objc_sync_exit(object)
 }
 
+/// Execute `block`, returning an object of type `T`, synchronized on `object`.
+/// Multiple blocks synchronized on the same `object`,
+/// but executing on different threads, will not run at the same time.
 func synchronized<T>(_ object: AnyObject, block: () -> T) -> T {
     objc_sync_enter(object)
     let result: T = block()
