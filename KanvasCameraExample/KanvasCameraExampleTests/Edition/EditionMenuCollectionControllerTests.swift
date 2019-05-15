29313bd183187daab8a5c30a80a00ec8fbe77894
Gabriel Mazzei <gmazzei91@gmail.com>
2019-05-15 13:19:34 -0300

    Editor screen - Tests

---
+++ KanvasCameraExample/KanvasCameraExampleTests/Edition/EditionMenuCollectionControllerTests.swift
@@ -0,0 +1,35 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+@testable import KanvasCamera
+import FBSnapshotTestCase
+import Foundation
+import UIKit
+import XCTest
+
+final class EditionMenuCollectionControllerTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.recordMode = false
+    }
+    
+    func newViewController() -> EditionMenuCollectionController {
+        let controller = EditionMenuCollectionController(settings: CameraSettings())
+        controller.view.frame = CGRect(x: 0, y: 0, width: 600, height: 600)
+        controller.view.setNeedsDisplay()
+        return controller
+    }
+    
+    func testCollectionControllerView() {
+        let controller = newViewController()
+        UIView.setAnimationsEnabled(false)
+        controller.showView(true)
+        UIView.setAnimationsEnabled(true)
+        FBSnapshotVerifyView(controller.view)
+    }
+}
