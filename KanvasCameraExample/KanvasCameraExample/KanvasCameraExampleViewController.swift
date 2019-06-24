42c0095e9fa840997874ec0e2f92b8537dc765e5
Jimmy Schementi <jimmys@tumblr.com>
2019-06-24 15:20:34 -0400

    Kanvas: disable the editor until it actually works ;)

--- KanvasCameraExample/KanvasCameraExample/KanvasCameraExampleViewController.swift
+++ KanvasCameraExample/KanvasCameraExample/KanvasCameraExampleViewController.swift
@@ -96,7 +96,7 @@ final class KanvasCameraExampleViewController: UIViewController {
         settings.features.openGLPreview = true
         settings.features.openGLCapture = true
         settings.features.cameraFilters = true
-        settings.features.editor = true
+        settings.features.editor = false
         settings.features.editorFilters = true
         settings.features.editorMedia = true
         return settings
