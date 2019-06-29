31d169aa33b799e7f0d1795f1d62ffbe91de90a4
Jimmy Schementi <jimmys@tumblr.com>
2019-06-28 22:47:51 -0400

    Kanvas: fix flakey media picker test
    A media picker test is flakey because the media picker button tries to
    load the most recent image in the photo library as the button's
    thumbnail, so it could be a different image, or not loaded yet. The
    current snapshot test has the image as not loaded.
    
    This adds the sample.png image to the library so the image is always the
    same, and then waits two seconds to ensure it's loaded. The test then
    deletes the image from the library.

--- KanvasCameraExample/KanvasCameraExampleTests/Camera/CameraControllerTests.swift
+++ KanvasCameraExample/KanvasCameraExampleTests/Camera/CameraControllerTests.swift
@@ -9,6 +9,7 @@ import FBSnapshotTestCase
 import Foundation
 import UIKit
 import XCTest
+import Photos
 
 final class CameraControllerTests: FBSnapshotTestCase {
 
@@ -230,12 +231,27 @@ final class CameraControllerTests: FBSnapshotTestCase {
     }
 
     func testCameraWithMediaPickerButton() {
+        var placeholder: PHObjectPlaceholder?
+        try? PHPhotoLibrary.shared().performChangesAndWait {
+            guard let image = Bundle(for: type(of: self)).path(forResource: "sample", ofType: "png").flatMap({ UIImage(contentsOfFile: $0) }) else { return }
+            let request = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            placeholder = request.placeholderForCreatedAsset
+        }
+        defer {
+            if let placeholder = placeholder {
+                try? PHPhotoLibrary.shared().performChangesAndWait {
+                    let result = PHAsset.fetchAssets(withLocalIdentifiers: [placeholder.localIdentifier], options: nil)
+                    PHAssetChangeRequest.deleteAssets(result)
+                }
+            }
+        }
         let settings = CameraSettings()
         settings.enabledModes = [.photo]
         settings.features.mediaPicking = true
         settings.features.cameraFilters = true
         let delegate = newDelegateStub()
         let controller = newController(delegate: delegate, settings: settings)
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 2.0))
         FBSnapshotVerifyView(controller.view)
     }
 
