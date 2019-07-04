1d1708b139b1d322d3fcde2cdce6e0f0992f530a
Gabriel Mazzei <gmazzei91@gmail.com>
2019-07-04 10:57:00 -0300

    New tests added

--- KanvasCameraExample/KanvasCameraExampleTests/Utility/ColorThief/MMCQTests.swift
+++ KanvasCameraExample/KanvasCameraExampleTests/Utility/ColorThief/MMCQTests.swift
@@ -24,4 +24,20 @@ final class MMCQTests: XCTestCase {
         
         XCTAssertEqual(colors, expectedColors, "Expected different colors")
     }
+    
+    func testVBox() {
+        let vbox = MMCQ.VBox(rMin: 0, rMax: 0, gMin: 0, gMax: 0, bMin: 0, bMax: 0, histogram: [0,0,0,0,0,0,0,0,0,0])
+        
+        let color = vbox.getAverage().makeUIColor()
+        let expectedColor = UIColor(hex: "#040404")
+        XCTAssertEqual(color, expectedColor, "Expected different colors")
+    }
+    
+    func testColorMap() {
+        let colorMap = MMCQ.ColorMap().makeNearestColor(to: MMCQ.Color(r: 0, g: 0, b: 0))
+        let color = colorMap.makeUIColor()
+        let expectedColor = UIColor(hex: "#000000")
+        
+        XCTAssertEqual(color, expectedColor, "Expected different colors")
+    }
 }
