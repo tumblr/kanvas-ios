3d72f9e80171b41c87c883034bc7560cdbaa5644
Gabriel Mazzei <gmazzei91@gmail.com>
2019-07-23 15:29:12 -0300

    ColorPicker - Tests - Empty classes

---
+++ KanvasCameraExample/KanvasCameraExampleTests/Editor/Drawing/ColorPicker/ColorPickerControllerTests.swift
@@ -0,0 +1,21 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+@testable import KanvasCamera
+import FBSnapshotTestCase
+import Foundation
+import UIKit
+import XCTest
+
+final class ColorPickerControllerTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.recordMode = false
+    }
+    
+}
---
+++ KanvasCameraExample/KanvasCameraExampleTests/Editor/Drawing/ColorPicker/ColorPickerViewTests.swift
@@ -0,0 +1,22 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+@testable import KanvasCamera
+import AVFoundation
+import FBSnapshotTestCase
+import Foundation
+import UIKit
+import XCTest
+
+final class StrokeSelectorViewTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.recordMode = false
+    }
+    
+}
