96c1a3ce1a2cee48447c81dca9f3273312ba510b
Gabriel Mazzei <gmazzei91@gmail.com>
2019-08-08 10:46:29 -0300

    print() deleted

--- KanvasCameraExample/KanvasCameraExampleTests/Extensions/UIImage+DominantColorsTests.swift
+++ KanvasCameraExample/KanvasCameraExampleTests/Extensions/UIImage+DominantColorsTests.swift
@@ -12,9 +12,7 @@ final class UIImageDominantColorsTests: XCTestCase {
     func testDominantColors() {
         guard let image = KanvasCameraImages.confirmImage else { return }
         let colors = image.getDominantColors(count: 3)
-        
-        print(colors.map { $0.hexString() })
-        
+                
         let expectedColors = [UIColor(hex: "#24bbfa"),
                               UIColor(hex: "#040505"),
                               UIColor(hex: "#f4f9fc"),
