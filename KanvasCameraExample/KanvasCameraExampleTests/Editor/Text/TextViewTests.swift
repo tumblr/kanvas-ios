fe5c0d2f69f1e4aa874bb6c4df6d6dc21fcc23d4
Gabriel Mazzei <gmazzei91@gmail.com>
2019-08-23 13:22:46 -0300

    Text tools tests

---
+++ KanvasCameraExample/KanvasCameraExampleTests/Editor/Text/TextViewTests.swift
@@ -0,0 +1,33 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+@testable import KanvasCamera
+import AVFoundation
+import FBSnapshotTestCase
+import Foundation
+import UIKit
+import XCTest
+
+final class TextViewTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.recordMode = false
+    }
+    
+    func newView() -> TextView {
+        let view = TextView()
+        view.frame = CGRect(x: 0, y: 0, width: 320, height: 480)
+        return view
+    }
+    
+    func testViewSetup() {
+        let view = newView()
+        FBSnapshotVerifyView(view)
+    }
+    
+}
