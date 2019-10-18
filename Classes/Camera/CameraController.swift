7713cb859b46382dbadb467f49497a7302f3305d
Jimmy Schementi <jimmys@tumblr.com>
2019-10-18 16:10:50 -0400

    Fix media picker button highlight

--- Classes/Camera/CameraController.swift
+++ Classes/Camera/CameraController.swift
@@ -257,7 +257,6 @@ public class CameraController: UIViewController, MediaClipsEditorDelegate, Camer
 
     override public func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        modeAndShootController.resetMediaPickerButton()
     }
 
     // MARK: - navigation
@@ -614,7 +613,9 @@ public class CameraController: UIViewController, MediaClipsEditorDelegate, Camer
         imagePickerController.sourceType = .savedPhotosAlbum
         imagePickerController.allowsEditing = false
         imagePickerController.mediaTypes = ["\(kUTTypeMovie)", "\(kUTTypeImage)"]
-        present(imagePickerController, animated: true, completion: nil)
+        present(imagePickerController, animated: true) {
+            self.modeAndShootController.resetMediaPickerButton()
+        }
         analyticsProvider?.logMediaPickerOpen()
     }
 
