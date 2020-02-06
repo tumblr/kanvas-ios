b78c66c4fbe05933e992b9c233f2bd192e3bc7af
Jimmy Schementi <jimmys@tumblr.com>
2020-02-06 18:47:02 -0500

    Fix GIF timing tests

--- KanvasCameraExample/KanvasCameraExampleTests/Constants/KanvasCameraTimesTests.swift
+++ KanvasCameraExample/KanvasCameraExampleTests/Constants/KanvasCameraTimesTests.swift
@@ -36,7 +36,12 @@ final class KanvasCameraTimesTests: XCTestCase {
     }
 
     func testGifHoldRecordingTime() {
-        XCTAssert(KanvasCameraTimes.gifHoldRecordingTime == 3, "Returned value does not match expected value")
+        if Device.isIPhone6P || Device.isIPhone6 {
+            XCTAssert(KanvasCameraTimes.gifTapRecordingTime == 1, "Returned value does not match expected value")
+        }
+        else {
+            XCTAssert(KanvasCameraTimes.gifHoldRecordingTime == 3, "Returned value does not match expected value")
+        }
     }
 
     func testGifPreferredFramesPerSecond() {
@@ -48,12 +53,23 @@ final class KanvasCameraTimesTests: XCTestCase {
     }
 
     func testGifHoldTotalFrames() {
-        XCTAssertEqual(KanvasCameraTimes.gifHoldNumberOfFrames, 30, "Returned value does not match expected value")
+        if Device.isIPhone6P || Device.isIPhone6 {
+            XCTAssertEqual(KanvasCameraTimes.gifHoldNumberOfFrames, 10, "Returned value does not match expected value")
+        }
+        else {
+            XCTAssertEqual(KanvasCameraTimes.gifHoldNumberOfFrames, 30, "Returned value does not match expected value")
+        }
     }
 
     func testGifRecordingTime() {
         XCTAssertEqual(KanvasCameraTimes.recordingTime(for: .gif, hold: false), 1, "Tapping the GIF shutter should record for 1 second")
-        XCTAssertEqual(KanvasCameraTimes.recordingTime(for: .gif, hold: true), 3, "Holding the GIF shutter should record for 2 seconds")
+        if Device.isIPhone6P || Device.isIPhone6 {
+            XCTAssertEqual(KanvasCameraTimes.recordingTime(for: .gif, hold: true), 1, "Holding the GIF shutter should record for 1 second")
+        }
+        else {
+            XCTAssertEqual(KanvasCameraTimes.recordingTime(for: .gif, hold: true), 3, "Holding the GIF shutter should record for 3 seconds")
+        }
+
     }
 
 }
