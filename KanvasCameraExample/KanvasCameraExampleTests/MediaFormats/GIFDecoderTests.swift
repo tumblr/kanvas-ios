2a5c23afea9f4fba45317f10006641f8310751c4
Jimmy Schementi <jimmys@tumblr.com>
2020-05-05 10:49:11 -0400

    test GIFDecoderImageIO directly to appease Gandalf

--- KanvasCameraExample/KanvasCameraExampleTests/MediaFormats/GIFDecoderTests.swift
+++ KanvasCameraExample/KanvasCameraExampleTests/MediaFormats/GIFDecoderTests.swift
@@ -21,15 +21,45 @@ final class GIFDecoderTests: FBSnapshotTestCase {
     }
 
     func testDecodeGIF() {
+
+        ///
+        /// Arrange
+        ///
+
         guard let gifURL = Bundle(for: type(of: self)).url(forResource: "colors", withExtension: "gif") else {
             XCTFail("Could not load colors.gif")
             return
         }
-        let view = UIImageView()
-        let expectedInterval = 0.15
-        GIFDecoderFactory.create(type: .imageIO).decode(image: gifURL) { frames in
-            XCTAssertTrue(frames.count == 13)
-            for (i, frame) in frames.enumerated() {
+
+        // These are the frame delays in colors.gif.
+        // These are all the same, but
+        let expectedIntervals = [
+            0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15
+        ]
+        let expectedFramesCount = expectedIntervals.count
+
+        ///
+        /// Act
+        ///
+
+        guard let decoder = GIFDecoderFactory.create(type: .imageIO) as? GIFDecoderImageIO else {
+            XCTFail("Expected decoder to be of type GIFDecoderImageIO")
+            return
+        }
+
+        // Doing this to directly test GIFDecoderImageIO so our testing tools
+        // know GIFDecoderImageIO is being tested.
+        let decode = GIFDecoderImageIO.decode(decoder)
+        decode(gifURL) { frames in
+
+            ///
+            /// Assert
+            ///
+
+            XCTAssertEqual(frames.count, expectedFramesCount)
+
+            let view = UIImageView()
+            for (i, (frame, expectedInterval)) in zip(frames, expectedIntervals).enumerated() {
                 XCTAssertEqual(frame.interval, expectedInterval)
                 view.frame = CGRect(x: 0, y: 0, width: frame.image.width, height: frame.image.height)
                 view.image = UIImage(cgImage: frame.image)
@@ -37,6 +67,6 @@ final class GIFDecoderTests: FBSnapshotTestCase {
                 self.FBSnapshotVerifyView(view, identifier: "\(i)")
             }
         }
-
     }
+
 }
