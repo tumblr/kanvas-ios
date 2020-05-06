dd779e36cc097fa95231f4f805b09d4d8b2e3149
Gabriel Mazzei <gmazzei91@gmail.com>
2020-05-06 17:48:19 -0300

    Improvement for text

--- Classes/Editor/Text/StylableTextView.swift
+++ Classes/Editor/Text/StylableTextView.swift
@@ -193,9 +193,50 @@ class StylableTextView: UITextView, UITextViewDelegate, MovableViewInnerElement 
     // MARK: - MovableViewInnerElement
     
     func hitInsideShape(point: CGPoint) -> Bool {
-        return highlightViews.contains { view in
-            return view.frame.contains(point)
+        var result = false
+        
+        for (i, view) in highlightViews.enumerated() {
+            let insideView = view.frame.contains(point)
+            
+            let inNextSpace: Bool
+            if let nextView = highlightViews.object(at: i+1) {
+                let spaceBetweenLines = [CGPoint(x: view.frame.minX, y: view.frame.maxY),
+                                         CGPoint(x: view.frame.maxX, y: view.frame.maxY),
+                                         CGPoint(x: nextView.frame.maxX, y: nextView.frame.minY),
+                                         CGPoint(x: nextView.frame.minX, y: nextView.frame.minY),]
+                
+                inNextSpace = contains(polygon: spaceBetweenLines, point: point)
+            }
+            else {
+                inNextSpace = false
+            }
+            
+            result = result || insideView || inNextSpace
         }
+        
+        return result
+    }
+    
+    /// Checks if a polygon contains a specific point
+    ///
+    /// - Parameters:
+    ///   - polygon: list of points to create the shape
+    ///   - point: point to be tested
+    private func contains(polygon: [CGPoint], point: CGPoint) -> Bool {
+        guard polygon.count > 1 else { return false }
+
+        let p = UIBezierPath()
+        let firstPoint = polygon[0]
+
+        p.move(to: firstPoint)
+
+        for index in 1...polygon.count-1 {
+            p.addLine(to: polygon[index])
+        }
+
+        p.close()
+
+       return p.contains(point)
     }
 }
 
