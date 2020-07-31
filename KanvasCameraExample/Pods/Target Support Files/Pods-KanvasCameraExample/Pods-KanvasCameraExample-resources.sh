21a9dc33a6f6ca0904f8ce468005ecec768a6b17
Brandon Titus <b@titus.io>
2020-07-30 21:54:51 -0600

    Removes ImageLoader dependency in Kanvas Example

--- KanvasCameraExample/Pods/Target Support Files/Pods-KanvasCameraExample/Pods-KanvasCameraExample-resources.sh
+++ KanvasCameraExample/Pods/Target Support Files/Pods-KanvasCameraExample/Pods-KanvasCameraExample-resources.sh
@@ -98,11 +98,9 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/KanvasCamera/KanvasCamera.bundle"
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/SharedUI/SharedUI.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/KanvasCamera/KanvasCamera.bundle"
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/SharedUI/SharedUI.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
--- KanvasCameraExample/Pods/Target Support Files/Utils/Utils-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Utils : NSObject
-@end
-@implementation PodsDummy_Utils
-@end
