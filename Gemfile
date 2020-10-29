4663c5ca134af4f0b0b0dd37cc824ea41ee05ee3
Brandon Titus <b@titus.io>
2020-10-29 15:37:30 -0600

    Switches Fastlane scripts for Building and Testing
    - Moves Build and Test step to Fastlane script (the results are a bit better with CircleCI)
    - Removes the Pods from KanvasCameraExample
    - Switches to the Cocoapods CDN source for faster builds
    - Removes the process.yml which is not needed

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org' do
   gem 'cocoapods', '1.9.1'
+  gem 'fastlane', '2.165.0'
 end
--- KanvasCameraExample/Pods/Target Support Files/KanvasCamera/KanvasCamera-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KanvasCamera : NSObject
-@end
-@implementation PodsDummy_KanvasCamera
-@end
