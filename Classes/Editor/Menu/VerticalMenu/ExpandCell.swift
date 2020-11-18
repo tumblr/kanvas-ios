d9fd1f8a180754e798a90eba200af5f833ea21ea
Gabriel Mazzei <gabriel.mazzei@tumblr.com>
2020-11-18 20:20:48 -0300

    Expand and collpase feature

---
+++ Classes/Editor/Menu/VerticalMenu/ExpandCell.swift
@@ -0,0 +1,160 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+import UIKit
+
+/// Delegate for touch events on this cell.
+protocol ExpandCellDelegate: class {
+    
+    /// Callback method when tapping a cell.
+    ///
+    /// - Parameters:
+    ///   - cell: the cell that was tapped
+    ///   - recognizer: the tap gesture recognizer
+    func didTap(cell: ExpandCell, recognizer: UITapGestureRecognizer)
+}
+
+private struct Constants {
+    static let circleDiameter: CGFloat = 48
+    static let labelHeight: CGFloat = 24
+    static let circleMargin: CGFloat = 4
+    
+    static let animationDuration: TimeInterval = 0.25
+    static let labelFont: UIFont = .boldSystemFont(ofSize: 16)
+    static let labelInset: CGFloat = 12
+    static let labelTextColor: UIColor = .white
+    static let backgroundColor: UIColor = UIColor.black.withAlphaComponent(0.4)
+    static let upAngle: CGFloat = -.pi / 2
+    static let downAngle: CGFloat = .pi / 2
+    
+    static var height: CGFloat {
+        return circleDiameter + 2 * circleMargin
+    }
+    
+    static var width: CGFloat {
+        return circleDiameter
+    }
+}
+
+/// The cell in VerticalMenuView to display an individual option.
+final class ExpandCell: UIView {
+    
+    static let height = Constants.height
+    static let width = Constants.width
+    
+    let iconView: UIImageView
+    private let label: UILabel
+    
+    weak var delegate: ExpandCellDelegate?
+        
+    init() {
+        iconView = UIImageView()
+        label = RoundedLabel()
+        super.init(frame: .zero)
+        setUpView()
+        setUpRecognizers()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    
+    private func setUpView() {
+        setupIconView()
+        setupLabel()
+        close()
+    }
+    
+    private func setupIconView() {
+        addSubview(iconView)
+        iconView.accessibilityIdentifier = "Vertical Menu Cell Icon View"
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.clipsToBounds = true
+        iconView.layer.cornerRadius = Constants.circleDiameter / 2
+        iconView.layer.masksToBounds = true
+        iconView.contentMode = .center
+        iconView.image = KanvasCameraImages.backImage
+        iconView.backgroundColor = Constants.backgroundColor
+        
+        NSLayoutConstraint.activate([
+            iconView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            iconView.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
+            iconView.heightAnchor.constraint(equalToConstant: Constants.circleDiameter),
+            iconView.widthAnchor.constraint(equalToConstant: Constants.circleDiameter)
+        ])
+    }
+    
+    private func setupLabel() {
+        addSubview(label)
+        label.accessibilityIdentifier = "Vertical Menu Cell Label"
+        label.textColor = Constants.labelTextColor
+        label.backgroundColor = Constants.backgroundColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.alpha = 0
+        
+        let leadingMargin = Constants.circleDiameter + Constants.circleMargin
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: leadingMargin),
+            label.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
+            label.heightAnchor.constraint(equalToConstant: Constants.labelHeight),
+        ])
+    }
+    
+    // MARK: - Gesture recognizers
+    
+    private func setUpRecognizers() {
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        addGestureRecognizer(tapRecognizer)
+    }
+    
+    @objc private func handleTap(recognizer: UITapGestureRecognizer) {
+        delegate?.didTap(cell: self, recognizer: recognizer)
+    }
+    
+    // MARK: - Public interface
+    
+    func open() {
+        label.text = "Close"
+        iconView.transform = CGAffineTransform(rotationAngle: Constants.downAngle)
+    }
+    
+    func close() {
+        label.text = "Open"
+        iconView.transform = CGAffineTransform(rotationAngle: Constants.upAngle)
+    }
+}
+
+/// Custom label with horizontal inset and rounded corners.
+private class RoundedLabel: UILabel {
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        font = Constants.labelFont
+        layer.cornerRadius = Constants.labelHeight / 2
+        layer.masksToBounds = true
+    }
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: 0, left: Constants.labelInset, bottom: 0, right: Constants.labelInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+        
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + Constants.labelInset * 2, height: size.height)
+    }
+}
--- Classes/Editor/Menu/VerticalMenu/VerticalMenuView.swift
+++ Classes/Editor/Menu/VerticalMenu/VerticalMenuView.swift
@@ -7,6 +7,11 @@
 import Foundation
 import UIKit
 
+private struct Constants {
+    static let animationDuration: TimeInterval = 0.5
+    static let maxVisibleCells: Int = 3
+}
+
 protocol VerticalMenuViewDelegate: class {
     
     func numberOfItems() -> Int
@@ -14,22 +19,50 @@ protocol VerticalMenuViewDelegate: class {
     func bindItem(at index: Int)
 }
 
+private enum State {
+    case open
+    case closed
+}
+
 /// Collection view for VerticalMenuController.
-final class VerticalMenuView: IgnoreTouchesView {
+final class VerticalMenuView: IgnoreTouchesView, ExpandCellDelegate {
     
     private weak var delegate: VerticalMenuViewDelegate?
     
+    private var state: State
     private let scrollView: UIScrollView
+    private let scrollViewContent: IgnoreTouchesView
     private let contentView: IgnoreTouchesView
+    private let fadeView: IgnoreTouchesView
     private var cells: [VerticalMenuCell]
+    private let expandCell: ExpandCell
+    
+    private var showExpandCell: Bool {
+        guard let delegate = delegate else { return false }
+        return delegate.numberOfItems() > Constants.maxVisibleCells
+    }
+    
+    private lazy var contentHeightContraint: NSLayoutConstraint = {
+        contentView.heightAnchor.constraint(equalToConstant: 0)
+    }()
+    
+    private lazy var fadeViewHeightContraint: NSLayoutConstraint = {
+        fadeView.heightAnchor.constraint(equalToConstant: 0)
+    }()
     
     init(delegate: VerticalMenuViewDelegate) {
         self.delegate = delegate
         self.scrollView = IgnoreTouchesScrollView()
+        self.scrollViewContent = IgnoreTouchesView()
         self.contentView = IgnoreTouchesView()
+        self.fadeView = IgnoreTouchesView()
+        self.expandCell = ExpandCell()
         self.cells = []
+        self.state = .closed
         super.init(frame: .zero)
-        setUpViews()
+        self.expandCell.delegate = self
+        
+        setupViews()
     }
     
     @available(*, unavailable, message: "use init() instead")
@@ -44,9 +77,9 @@ final class VerticalMenuView: IgnoreTouchesView {
     
     // MARK: - Layout
     
-    private func setUpViews() {
+    private func setupViews() {
         setupScrollView()
-        setupContentView()
+        setupScrollViewContent()
     }
     
     private func setupScrollView() {
@@ -68,23 +101,62 @@ final class VerticalMenuView: IgnoreTouchesView {
         ])
     }
     
-    private func setupContentView() {
-        scrollView.addSubview(contentView)
-        contentView.translatesAutoresizingMaskIntoConstraints = false
+    private func setupScrollViewContent() {
+        scrollView.addSubview(scrollViewContent)
+        scrollViewContent.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            contentView.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor),
-            contentView.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor),
+            scrollViewContent.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor),
+            scrollViewContent.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor),
+            scrollViewContent.trailingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.trailingAnchor),
         ])
     }
+       
+    private func setupContentView() {
+        guard let delegate = delegate else { return }
         
+        scrollViewContent.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let numberOfItems = CGFloat(delegate.numberOfItems())
+        
+        let itemHeight: CGFloat = numberOfItems * VerticalMenuCell.height
+        let expandCellHeight = ExpandCell.height
+        let contentHeight: CGFloat = showExpandCell ? (itemHeight + expandCellHeight) : itemHeight
+        
+        contentHeightContraint.constant = contentHeight
+        NSLayoutConstraint.activate([
+            contentHeightContraint,
+            contentView.centerYAnchor.constraint(equalTo: scrollViewContent.safeAreaLayoutGuide.centerYAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollViewContent.safeAreaLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollViewContent.safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
+    
+    private func setupFadeView() {
+        guard let delegate = delegate else { return }
+        
+        contentView.addSubview(fadeView)
+        fadeView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let numberOfItems = CGFloat(delegate.numberOfItems())
+        let contentHeight: CGFloat = numberOfItems * VerticalMenuCell.height
+        
+        fadeViewHeightContraint.constant = contentHeight
+        NSLayoutConstraint.activate([
+            fadeViewHeightContraint,
+            fadeView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+            fadeView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
+            fadeView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
+    
     private func setupCollection() {
         guard let delegate = delegate else { return }
         
         for _ in 0..<delegate.numberOfItems() {
             let cell = VerticalMenuCell()
-            contentView.addSubview(cell)
+            fadeView.addSubview(cell)
             cells.append(cell)
         }
     }
@@ -92,20 +164,30 @@ final class VerticalMenuView: IgnoreTouchesView {
     private func setupCells() {
         guard let delegate = delegate else { return }
         
-        let contentHeight: CGFloat = CGFloat(cells.count) * VerticalMenuCell.height
-        
         for index in 0..<delegate.numberOfItems() {
             let cell = cells[index]
             cell.translatesAutoresizingMaskIntoConstraints = false
             
-            let centerYOffset: CGFloat = -(contentHeight / 2) + VerticalMenuCell.height * CGFloat(index)
+            let topOffset: CGFloat = VerticalMenuCell.height * CGFloat(index)
             NSLayoutConstraint.activate([
-                cell.centerYAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerYAnchor, constant: centerYOffset),
-                cell.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor),
+                cell.topAnchor.constraint(equalTo: fadeView.safeAreaLayoutGuide.topAnchor, constant: topOffset),
+                cell.centerXAnchor.constraint(equalTo: fadeView.safeAreaLayoutGuide.centerXAnchor),
                 cell.heightAnchor.constraint(equalToConstant: VerticalMenuCell.height),
                 cell.widthAnchor.constraint(equalToConstant: VerticalMenuCell.width),
             ])
         }
+    }
+    
+    private func setupExpandCell() {
+        contentView.addSubview(expandCell)
+        expandCell.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            expandCell.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor),
+            expandCell.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor),
+            expandCell.heightAnchor.constraint(equalToConstant: ExpandCell.height),
+            expandCell.widthAnchor.constraint(equalToConstant: ExpandCell.width),
+        ])
     }
     
     private func bindCells() {
@@ -117,20 +199,113 @@ final class VerticalMenuView: IgnoreTouchesView {
     }
     
     private func resetCollection() {
+        expandCell.removeFromSuperview()
+        
         cells.forEach { cell in
             cell.removeFromSuperview()
         }
         
         cells.removeAll()
+        contentHeightContraint.constant = 0
+        fadeViewHeightContraint.constant = 0
+    }
+    
+    // MARK: - Expand & Collapse
+    
+    private func moveExpandCellUp() {
+        contentHeightContraint.constant = VerticalMenuCell.height * CGFloat(Constants.maxVisibleCells) + ExpandCell.height
+        fadeViewHeightContraint.constant = VerticalMenuCell.height * CGFloat(Constants.maxVisibleCells)
+    }
+    
+    private func moveExpandCellDown() {
+        guard let delegate = delegate else { return }
+        contentHeightContraint.constant = VerticalMenuCell.height * CGFloat(delegate.numberOfItems()) + ExpandCell.height
+        fadeViewHeightContraint.constant = VerticalMenuCell.height * CGFloat(delegate.numberOfItems())
+    }
+    
+    private func hideExtraCells() {
+        let extraCells = cells[Constants.maxVisibleCells..<cells.count]
+        extraCells.forEach {
+            $0.alpha = 0
+        }
+    }
+    
+    private func showExtraCells() {
+        let extraCells = cells[Constants.maxVisibleCells..<cells.count]
+        extraCells.forEach {
+            $0.alpha = 1
+        }
     }
     
     // MARK: - Public interface
     
+    func collapseCollection(animated: Bool = false) {
+        state = .closed
+        let firstAction: () -> Void = { [weak self] in
+            guard let self = self else { return }
+            self.hideExtraCells()
+        }
+        
+        let secondAction: () -> Void = { [weak self] in
+            guard let self = self else { return }
+            self.expandCell.close()
+            self.moveExpandCellUp()
+            self.layoutIfNeeded()
+        }
+        
+        if animated {
+            let duration = Constants.animationDuration
+            UIView.animateKeyframes(withDuration: duration, delay: 0, options: [.calculationModeCubic], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.1 / duration, animations: firstAction)
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5 / duration, animations: secondAction)
+            }, completion: nil)
+        }
+        else {
+            firstAction()
+            secondAction()
+        }
+    }
+    
+    func expandCollection(animated: Bool = false) {
+        state = .open
+        
+        let firstAction: () -> Void = { [weak self] in
+            guard let self = self else { return }
+            self.expandCell.open()
+            self.moveExpandCellDown()
+            self.layoutIfNeeded()
+        }
+        
+        let secondAction: () -> Void = { [weak self] in
+            guard let self = self else { return }
+            self.showExtraCells()
+        }
+        
+        if animated {
+            let duration = Constants.animationDuration
+            UIView.animateKeyframes(withDuration: duration, delay: 0, options: [.calculationModeCubic], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0 / duration, relativeDuration: 0.5 / duration, animations: firstAction)
+                UIView.addKeyframe(withRelativeStartTime: 0.1 / duration, relativeDuration: 0.4 / duration, animations: secondAction)
+            }, completion: nil)
+        }
+        else {
+            firstAction()
+            secondAction()
+        }
+    }
+    
     func reload() {
         resetCollection()
+        setupFadeView()
+        setupContentView()
         setupCollection()
         setupCells()
         bindCells()
+        
+        if showExpandCell {
+            setupExpandCell()
+            collapseCollection()
+        }
     }
     
     func getCell(at index: Int) -> VerticalMenuCell? {
@@ -143,7 +318,18 @@ final class VerticalMenuView: IgnoreTouchesView {
     
     func reloadItem(at index: Int) {
         delegate?.bindItem(at: index)
-    }    
+    }
+    
+    // MARK: - ExpandCellDelegate
+    
+    func didTap(cell: ExpandCell, recognizer: UITapGestureRecognizer) {
+        switch state {
+        case .open:
+            collapseCollection(animated: true)
+        case .closed:
+            expandCollection(animated: true)
+        }
+    }
 }
 
 private final class IgnoreTouchesScrollView: UIScrollView {
@@ -152,5 +338,4 @@ private final class IgnoreTouchesScrollView: UIScrollView {
         let hitView = super.hitTest(point, with: event)
         return hitView == self ? nil : hitView
     }
-    
 }
