0bce1d89aa9861923fce6ab3cb7eebd608f7b0b2
Brandon Titus <b@titus.io>
2021-02-08 19:44:27 -0700

    Remove Pod files + ignore in git

--- KanvasExample/Pods/Target Support Files/Kanvas/Kanvas-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Kanvas : NSObject
-@end
-@implementation PodsDummy_Kanvas
-@end
--- KanvasExample/Pods/Target Support Files/Pods-KanvasExampleTests/Pods-KanvasExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KanvasExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_KanvasExampleTests
-@end
