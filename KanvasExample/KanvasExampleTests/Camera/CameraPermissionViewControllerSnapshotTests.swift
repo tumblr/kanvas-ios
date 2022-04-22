79ea47bee1e558444889fee83a0111710db44d1c
Declan McKenna <deco354@gmail.com>
2022-04-22 10:45:28 +0300

    Create FBSnapshotArchFriendly extension
    This is to enable snapshots to pass on M1 and intel run simulators

--- KanvasExample/KanvasExampleTests/Camera/CameraPermissionViewControllerSnapshotTests.swift
+++ KanvasExample/KanvasExampleTests/Camera/CameraPermissionViewControllerSnapshotTests.swift
@@ -11,7 +11,6 @@ import XCTest
 import FBSnapshotTestCase
 
 final class CameraPermissionsViewControllerSnapshotTests: FBSnapshotTestCase {
-    private let arm64AndIntelCompatabilityPixelTolerance: CGFloat = 0.02
     private var mockDelegate: MockCameraPermissionsViewControllerDelegate { MockCameraPermissionsViewControllerDelegate() }
 
     override func setUp() {
@@ -23,21 +22,21 @@ final class CameraPermissionsViewControllerSnapshotTests: FBSnapshotTestCase {
         let authorizerMock = MockCaptureDeviceAuthorizer(initialCameraAccess: .authorized,
                                                          initialMicrophoneAccess: .authorized)
         let controller = CameraPermissionsViewController(captureDeviceAuthorizer: authorizerMock, delegate: mockDelegate)
-        FBSnapshotVerifyViewController(controller)
+        FBSnapshotArchFriendlyVerifyViewController(controller)
     }
     
     func testViewWithNoAccessDisplaysSettingsPrompt() {
         let authorizerMock = MockCaptureDeviceAuthorizer(initialCameraAccess: .denied,
                                                          initialMicrophoneAccess: .denied)
         let controller = CameraPermissionsViewController(captureDeviceAuthorizer: authorizerMock, delegate: mockDelegate)
-        FBSnapshotVerifyViewController(controller, perPixelTolerance: arm64AndIntelCompatabilityPixelTolerance)
+        FBSnapshotArchFriendlyVerifyViewController(controller)
     }
     
     func testViewWithUndeterminedAccessDisplaysSettingsPrompt() {
         let authorizerMock = MockCaptureDeviceAuthorizer(initialCameraAccess: .notDetermined,
                                                          initialMicrophoneAccess: .notDetermined)
         let controller = CameraPermissionsViewController(captureDeviceAuthorizer: authorizerMock, delegate: mockDelegate)
-        FBSnapshotVerifyViewController(controller, perPixelTolerance: arm64AndIntelCompatabilityPixelTolerance)
+        FBSnapshotArchFriendlyVerifyViewController(controller)
     }
     
     func testViewWithCameraOnlyAccessDisplaysSettingsPrompt() {
@@ -45,13 +44,24 @@ final class CameraPermissionsViewControllerSnapshotTests: FBSnapshotTestCase {
                                                          initialMicrophoneAccess: .notDetermined)
         let cameraOnlyAccessController = CameraPermissionsViewController(captureDeviceAuthorizer: authorizedCameraMock,
                                                                          delegate: mockDelegate)
-        FBSnapshotVerifyViewController(cameraOnlyAccessController)
+        FBSnapshotArchFriendlyVerifyViewController(cameraOnlyAccessController)
     }
     
     func testViewWithMicrophoneOnlyAccessDisplaysSettingsPrompt() {
         let authorizedMicMock = MockCaptureDeviceAuthorizer(initialCameraAccess: .denied, initialMicrophoneAccess: .authorized)
         let micOnlyAccessController = CameraPermissionsViewController(captureDeviceAuthorizer: authorizedMicMock,
                                                                       delegate: mockDelegate)
-        FBSnapshotVerifyViewController(micOnlyAccessController)
+        FBSnapshotArchFriendlyVerifyViewController(micOnlyAccessController)
+    }
+}
+
+fileprivate let defaultArm64CompatiblePerPixelTolerance: CGFloat = 0.02
+fileprivate let defaultArm64CompatibleOverallTolerance: CGFloat = 0.002
+
+private extension FBSnapshotTestCase {
+    func FBSnapshotArchFriendlyVerifyViewController(_ viewController: UIViewController,
+                                                    perPixelTolerance: CGFloat = defaultArm64CompatiblePerPixelTolerance,
+                                                    overallTolerance:CGFloat = defaultArm64CompatibleOverallTolerance) {
+        FBSnapshotVerifyViewController(viewController, perPixelTolerance: perPixelTolerance, overallTolerance: overallTolerance)
     }
 }
