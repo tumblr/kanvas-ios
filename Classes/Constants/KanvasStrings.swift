8b9ef524ba78ca77491689a1b65ca6cb3a5afa52
Daniel Bernal <daniel.bernal@automattic.com>
2022-06-24 20:05:20 +0200

    Use default values in new translations

--- Classes/Constants/KanvasStrings.swift
+++ Classes/Constants/KanvasStrings.swift
@@ -50,13 +50,11 @@ public struct KanvasStrings {
                                              cameraPermissionsDescriptionLabel: cameraPermissionDescriptionString)
     
     private static let cameraPermissionTitleString = {
-        NSLocalizedString("CameraAccessNoAccessTitle",
-                          comment: "Title text for scenerio when access to Photos has been disallowed")
+        NSLocalizedString("CameraAccessNoAccessTitle", value: "Please allow access to your camera and microphone", comment: "Title text for scenerio when access to Camera and Microphone is not allowed")
     }()
     
     private static let cameraPermissionDescriptionString = {
-        NSLocalizedString("CameraAccessNoAccessDesc",
-                          comment: "Description text for scenerio when access to Photos has been disallowed")
+        NSLocalizedString("CameraAccessNoAccessDesc", value: "This functionality is required to post videos and ", comment: "Description text for scenerio when access to Photos has been disallowed")        
     }()
 
     public init(cameraPermissionsTitleLabel: String,
